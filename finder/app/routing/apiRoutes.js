7f0c688b246e76e4d0da37ab9b3788f318cc004d
Christian Gingrich <xian777@gmail.com>
2018-04-27 20:17:46 -0700

    added routes

---
+++ finder/app/routing/apiRoutes.js
@@ -0,0 +1,26 @@
+app.get("/api/friends", function(req, res) {
+    var chosen = req.params.friends;
+
+    if (chosen) {
+        console.log(chosen);
+
+        for (var i = 0; i < friends.length; i++) {
+            if (chosen === friends[i].routeName) {
+                return res.json(friends[i]);
+            }
+        }
+        return res.json(false);
+    }
+    return res.json(friends);
+});
+
+// app.post("/api/friends", function(req, res) {
+//     var newfriend = req.body;
+//     newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
+
+//     console.log(newfriend);
+
+//     friends.push(newfriend);
+
+//     res.json(newfriend);
+//   });
---
+++ finder/app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+// GET route to /survey
+//default route that leads to home.html
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
